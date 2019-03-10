02ee904fb30d6e10bf9d3e1589c6239c4e71a831
golzar <a.golzarzadeh@gmail.com>
2019-03-10 14:24:58 +0330

    wip

--- Zanis.Ostad.Web/ClientApp/components/teacher/listCourse/index.vue
+++ Zanis.Ostad.Web/ClientApp/components/teacher/listCourse/index.vue
@@ -2,61 +2,63 @@
   <el-card>
     <div>
       <el-button>افزودن دوره جدید</el-button>
-      <addCourse ></addCourse>
+      <addCourse></addCourse>
     </div>
     <el-table height="500" :data="courceData" size="large" style="width: 100%">
       <el-table-column label="ردیف">
         <template slot-scope="scope">{{scope.row.id}}
-</template>
+        </template>
       </el-table-column>
 
-        <el-table-column label="عنوان">
-<template slot-scope="scope">
-  {{ scope.row.title}}
-</template>
+      <el-table-column label="عنوان">
+        <template slot-scope="scope">
+          {{ scope.row.title}}
+        </template>
       </el-table-column>
 
       <el-table-column label="کددرس">
-<template slot-scope="scope">
-  {{ scope.row.lessonCode}}
-</template>
+        <template slot-scope="scope">
+          {{ scope.row.lessonCode}}
+        </template>
       </el-table-column>
 
       <el-table-column label="قیمت">
-<template slot-scope="scope">
-  {{scope.row.price}}
-</template>
+        <template slot-scope="scope">
+          {{scope.row.price}}
+        </template>
       </el-table-column>
 
-    
 
       <el-table-column label="مقطع">
-<template slot-scope="scope">
-  {{ scope.row.gradeTitle}}
-</template>
+        <template slot-scope="scope">
+          {{ scope.row.gradeTitle}}
+        </template>
       </el-table-column>
       <el-table-column label="نام استاد">
-<template slot-scope="scope">
-  {{ scope.row.teacher}}
-</template>
+        <template slot-scope="scope">
+          {{ scope.row.teacher}}
+        </template>
       </el-table-column>
 
       <el-table-column label="جزئیات">
-<template slot-scope="scope">
-  <el-button @click="showdetails(scope.row.id)">
-    مشاهده</el-button>
-</template>
+        <template slot-scope="scope">
+          <el-button @click="showdetails(scope.row.id)">
+            مشاهده
+          </el-button>
+        </template>
       </el-table-column>
 
-       <el-table-column label="امکانات">
+      <el-table-column label="امکانات">
         <template slot-scope="scope">
-         <el-row type="flex">
+          <el-row type="flex">
             <el-button @click="activeSection(scope.row.id)">
-            فعال</el-button>
+              فعال
+            </el-button>
 
             <el-button @click="deActiveSection(scope.row.id)" class="deactive">
-            غیر فعال</el-button>
-         </el-row>
+              غیر فعال
+            </el-button>
+          </el-row>
         </template>
       </el-table-column>
 
@@ -72,7 +74,7 @@
       :total="meta.allCount"
     ></el-pagination>
 
-    <el-dialog title="جزئیات دوره" :visible.sync="courcedialog" width="80%">
+    <el-dialog title="جزئیات دوره" :visible.sync="!!courcedetails" width="80%">
       <el-row :gutter="40">
         <el-col :xs="12" :md="12" :lg="12" v-for="(item,index) in courcedetails" :key="index">
           <el-card class="card-item">
@@ -81,10 +83,10 @@
               <span class="icon" v-html="previewIconCourse(item.contentType)"></span>
             </div>
 
-            <div class="wrapper-body-card">در این بخش توضیحات  قرار می گیرد</div>
+            <div class="wrapper-body-card">در این بخش توضیحات قرار می گیرد</div>
 
             <div class="wrapper-download-link">
-             
+
 
               <a :href="''+item.filePath+''" class="download">دانلود</a>
             </div>
@@ -96,8 +98,9 @@
 </template>
 
 <script>
-import addCourse from './add-course-dialog';
+  import addCourse from './add-course-dialog';
   import axios from "axios";
+
   export default {
     name: "AdminListCourse",
     data() {
@@ -110,9 +113,9 @@ import addCourse from './add-course-dialog';
         courcedialog: false,
         meta: {}
       };
-       components:{
-         addCourse
-       }
+      components:{
+        addCourse
+      }
     },
     methods: {
       Getcourse() {
@@ -133,7 +136,7 @@ import addCourse from './add-course-dialog';
         this.query.currentPage = val;
         this.Getcourse();
       },
-  
+
       showdetails(id) {
         axios
           .get("/api/Courses/" + id)
@@ -141,51 +144,50 @@ import addCourse from './add-course-dialog';
             this.courcedetails = res.data.contents;
             this.courcedialog = true;
             console.log(res.data.contents);
-                    })
-          .catch(err => {});
+          })
+          .catch(err => {
+          });
       },
       previewIconCourse(contentType) {
-      switch (contentType) {
+        switch (contentType) {
           case 0:
             return '<i class="far fa-file-pdf"></i>';
           case 1:
             return '<i class="far fa-file-video"></i>';
-            default:
+          default:
             return '';
         }
       },
-      activeSection(id){
-        axios.patch('/api/TeacherAccount/courses/'+id+'/active')
-        .then(res=>{
-        this.$message({
-          message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
-          type: 'success'
-        });
-        })
-        .catch(err=>{
+      activeSection(id) {
+        axios.patch('/api/TeacherAccount/courses/' + id + '/active')
+          .then(res => {
+            this.$message({
+              message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
+              type: 'success'
+            });
+          })
+          .catch(err => {
 
-        })
+          })
 
       },
-      deActiveSection(id){
+      deActiveSection(id) {
 
-         axios.patch('/api/TeacherAccount/courses/'+id+'/deactive')
-        .then(res=>{
-          this.$message({
-          message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
-          type: 'warning'
-        });
-        console.log(res.data);
-        })
-        .catch(err=>{
+        axios.patch('/api/TeacherAccount/courses/' + id + '/deactive')
+          .then(res => {
+            this.$message({
+              message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
+              type: 'warning'
+            });
+            console.log(res.data);
+          })
+          .catch(err => {
 
-        })
+          })
 
       }
     },
-    computed: {
-      
-    },
+    computed: {},
     mounted() {
       this.Getcourse();
     }
@@ -196,17 +198,17 @@ import addCourse from './add-course-dialog';
   .icon {
     float: left;
   }
-  
+
   .download {
     color: #000;
     float: left;
   }
-  
-  .card-item{
+
+  .card-item {
     margin-bottom: 50px;
   }
 
-  .deactive{
+  .deactive {
     margin-right: 0;
   }
 
