356dac205ba59da2edbc7a82c9061ef191d31dd6
SyedAliHossini <salihoosainione@gmail.com>
2019-03-12 16:19:54 +0330

    wip

--- Zanis.Ostad.Web/ClientApp/components/teacher/courses/add-course-dialog.vue
+++ Zanis.Ostad.Web/ClientApp/components/teacher/courses/add-course-dialog.vue
@@ -1,131 +1,79 @@
 <template>
   <div>
     <el-dialog title="افزودن دوره" :visible.sync="isOpen" width="80%" @closed="$emit('close')">
-      <el-form>
-        <el-form-item label="قیمت">
-          <el-input placeholder="قیمت" v-model="price"></el-input>
-        </el-form-item>
+      <el-row>
+        <el-col :md="12">
+          <el-form>
+            <el-form-item label="قیمت">
+              <el-input placeholder="قیمت" v-model="form.price"></el-input>
+            </el-form-item>
 
-        <el-form-item label="توضیحات">
-          <el-input placeholder="توضیحات" v-model="description"></el-input>
-        </el-form-item>
+            <el-form-item label="توضیحات">
+              <el-input placeholder="توضیحات" v-model="form.description"></el-input>
+            </el-form-item>
 
-        <el-form-item label="عناوین">
-          <el-select v-model="courseTitle" placeholder="عناوین" class="w100">
-            <el-option
-              v-for="item in courseTitles"
-              :key="item.value"
-              :label="item.name"
-              :value="item.id"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button class="w100" @click="isLessonsearchDialog = true">انتخاب درس</el-button>
-        </el-form-item>
-        <el-form-item label="پیام به مدیریت">
-          <el-input type="textarea" v-model="teacherMessage"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-upload
-            class="upload-demo"
-            ref="upload"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :auto-upload="false"
-          >
-            <el-button slot="trigger" size="small">انتخاب فایل</el-button>
-            <el-button
-              style="margin-left: 10px;"
-              size="small"
-              type="success"
-              @click="submitUpload"
-            >ارسال</el-button>
-          </el-upload>
-        </el-form-item>
-      </el-form>
-      <el-button @click="registerPeriod">ثبت دوره</el-button>
+            <el-form-item label="عناوین">
+              <el-select v-model="form.courseTitle" placeholder="عناوین" class="w100">
+                <el-option
+                  v-for="item in courseTitles"
+                  :key="item.value"
+                  :label="item.name"
+                  :value="item.id"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button class="w100" @click="isLessonsearchDialog = true">انتخاب درس</el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-tag
+                type="danger"
+                v-if="itemSelectedLesson==''"
+              >در حال حاظر درسی را انتخاب نکرده اید</el-tag>
+              <el-tag v-else>{{itemSelectedLesson}}</el-tag>
+            </el-form-item>
+            <el-form-item label="پیام به مدیریت">
+              <el-input type="textarea" v-model="form.teacherMessage"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+              <input type="file" name="myfile" @change="processFile">
+              <br>
+              <el-button
+                style="margin-left: 10px;"
+                size="small"
+                type="success"
+                @click="registerLesson"
+              >ارسال</el-button>
+              <el-progress :percentage="fileUploadPercentCompleted"></el-progress>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <el-col :md="12">
+          <el-alert
+            title="توضیحات"
+            type="info"
+            description="در این بخش توضیحات قرار می گیرد"
+            :closable="close"
+            show-icon
+          ></el-alert>
+        </el-col>
+      </el-row>
     </el-dialog>
 
     <!-- searchLessonItem -->
-    <el-dialog title="جستجو" :visible.sync="isLessonsearchDialog" width="46%" @change="lessonSelected">
-      <el-container>
-        <el-row :gutter="5" type="flex">
-          <el-col :md="12">
-            <el-form>
-              <el-form-item>
-                <el-input placeholder="کلمه" v-model="termSearch"></el-input>
-              </el-form-item>
-            </el-form>
-          </el-col>
-          <el-col :md="12">
-            <el-select v-model="selectedGrade" filterable placeholder="مقطع">
-              <el-option
-                v-for="item in gradeItems"
-                :key="item.value"
-                :label="item.name"
-                :value="item.id"
-              ></el-option>
-            </el-select>
-          </el-col>
-          <el-col :md="12">
-            <el-select
-              @change="getLessonFields"
-              v-model="fieldId"
-              filterable
-              remote
-              reserve-keyword
-              placeholder="رشته"
-              :remote-method="getFieldItems"
-              :loading="loading"
-            >
-              <el-option
-                v-for="item in fieldItems"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              >
-                {{item.name}}
-                -
-                <span
-                  style="color:#ff8787;font-size:12px"
-                >{{item.gradeName}}</span>
-              </el-option>
-            </el-select>
-          </el-col>
-        </el-row>
-        <br>
-      </el-container>
-      <!-- lessonItems -->
-      <el-table :data="lessonItems" style="width: 100%">
-        <el-table-column width="300" label="نام درس">
-          <template slot-scope="scope">{{ scope.row.lessonName }}</template>
-        </el-table-column>
-
-        <el-table-column width="130" label="مقطع">
-          <template slot-scope="scope">{{ scope.row.gradeName }}</template>
-        </el-table-column>
-
-        <el-table-column width="130" label="رشته">
-          <template slot-scope="scope">{{ scope.row.fieldName }}</template>
-        </el-table-column>
-
-        <el-table-column width="130" label="عملیات">
-          <template slot-scope="scope">
-            <el-button
-              @click="selectLessonItem(scope.row)"
-              type="primary"
-              class="white"
-              plain
-            >انتخاب</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-dialog>
+    <searchLesson
+      :isOpen="isLessonsearchDialog"
+      @close="selectedLesson=undefined"
+      v-on:lessonSelected="getItems($event)"
+      v-on:close="closeSearchgDialog"
+    ></searchLesson>
   </div>
 </template>
 <script>
 import axios from "axios";
 
+import searchLesson from "./search-lesson";
 export default {
   name: "add-course-dialog",
   props: {
@@ -133,21 +81,27 @@ export default {
       type: Boolean
     }
   },
+  components: {
+    searchLesson
+  },
   data() {
     return {
-      price: "",
-      description: "",
-      courseTitle: "",
-      teacherMessage: "",
-      lessonFieldId: "",
       isLessonsearchDialog: false,
-      termSearch: "",
-      selectedGrade: "",
-      fieldId: "",
+      selectedTeacherItem: "",
+      itemSelectedLesson: "",
+      selectedLesson: null,
+      close: false,
+      progressCount: 0,
+      fileUploadPercentCompleted: undefined,
       courseTitles: [],
-      gradeItems: [],
-      fieldItems: [],
-      lessonItems: []
+      form: {
+        price: 0,
+        description: "",
+        courseTitle: "",
+        teacherMessage: "",
+        lessonFieldId: 0,
+        zipFile: ""
+      }
     };
   },
 
@@ -157,57 +111,45 @@ export default {
         this.courseTitles = res.data;
       });
     },
-    getTermGradeInput(term) {
-      axios
-        .get("/api/GradeFieldLessons/with-details", { params: { term } })
-        .then(res => {
-          // console.log(this.fieldsItemLessons);
-        });
+    getItems(item) {
+      this.itemSelectedLesson = item.fieldName;
+      this.form.lessonFieldId = item.id;
     },
-    // getGradeItems
-    getGradeItems() {
-      axios.get("/api/Grades").then(res => {
-        this.gradeItems = res.data;
+    closeSearchgDialog() {
+      this.isLessonsearchDialog = false;
+    },
+    processFile(event) {
+      this.form.zipFile = event.target.files[0];
+    },
+    registerLesson() {
+      const config = {
+        onUploadProgress: function(progressEvent) {
+          this.fileUploadPercentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+        }
+      };
+      let data = new FormData();
+      data.append("Price", this.form.price);
+      data.append("Description", this.form.description);
+      data.append("TeacherMessageForAdmin", this.form.teacherMessage);
+      data.append("CourseTitleId", this.form.courseTitle);
+      data.append("LessonFieldId", this.form.lessonFieldId);
+      data.append("ZipFile", this.form.zipFile);
+      axios.post("/api/TeacherAccount/courses", data, config).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            message: "دوره شما با موفقیت ثبت شد",
+            type: "success"
+          });
+          this.isLessonsearchDialog = false;
+        }
       });
-    },
-    getFieldItems(fieldSearchTearm) {
-      axios
-        .get("/api/Fields", {
-          params: { GradeId: this.selectedGrade, SearchText: fieldSearchTearm }
-        })
-        .then(res => {
-          this.fieldItems = res.data;
-          console.log(this.fieldItems);
-        });
-    },
-    getlessons() {
-      axios
-        .get("/api/GradeFieldLessons/with-details", {
-          params: {
-            Term: this.termSearch,
-            FieldId: this.fieldId,
-            GradeId: this.selectedGrade
-          }
-        })
-        .then(res => {
-          this.lessonItems = res.data;
-          console.log(res.data);
-        });
-    },
-    // changeFieldItem
-    getLessonFields() {
-      this.getlessons();
-    },
-    selectLessonItem(lessonItem) {
-      this.$emit("lessonSelected", this.handel);
-    },
-    registerPeriod() {
-      //let data = new FormData();
     }
   },
+
   mounted() {
     this.getCourseTitle();
-    this.getGradeItems();
   }
 };
 </script>
@@ -224,6 +166,18 @@ export default {
 }
 .white {
   color: #fff !important;
+}
+.el-tag {
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+}
+.description-Added-Course {
+  margin-right: 16px;
+}
+.description-Added-Course .title-Description {
+  text-align: justify;
+  line-height: 10px;
 }
 </style>
 
