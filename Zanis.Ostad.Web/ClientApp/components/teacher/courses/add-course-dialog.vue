a09f664ff7e36fa5230a36303caf90e3b9b0af9c
SyedAliHossini <salihoosainione@gmail.com>
2019-03-13 15:17:23 +0330

    wip

--- Zanis.Ostad.Web/ClientApp/components/teacher/courses/add-course-dialog.vue
+++ Zanis.Ostad.Web/ClientApp/components/teacher/courses/add-course-dialog.vue
@@ -1,203 +1,350 @@
 <template>
   <div>
+  
     <el-dialog title="افزودن دوره" :visible.sync="isOpen" width="80%" @closed="$emit('close')">
+  
       <el-row>
+  
         <el-col :md="12">
+  
           <el-form>
+  
             <el-progress v-show="uploadProgress" :percentage="uploadProgress"></el-progress>
+  
             <el-form-item label="قیمت">
+  
               <el-input placeholder="قیمت" v-model="form.price"></el-input>
+  
             </el-form-item>
-
+  
+  
+  
             <el-form-item label="توضیحات">
+  
               <el-input placeholder="توضیحات" v-model="form.description"></el-input>
+  
             </el-form-item>
-
+  
+  
+  
             <el-form-item label="عناوین">
+  
               <el-select v-model="form.courseTitle" placeholder="عناوین" class="w100">
-                <el-option
-                  v-for="item in courseTitles"
-                  :key="item.value"
-                  :label="item.name"
-                  :value="item.id"
-                ></el-option>
+  
+                <el-option v-for="item in courseTitles" :key="item.value" :label="item.name" :value="item.id"></el-option>
+  
               </el-select>
+  
             </el-form-item>
+  
             <el-form-item>
+  
               <el-button class="w100" @click="isLessonsearchDialog = true">انتخاب درس</el-button>
+  
             </el-form-item>
+  
             <el-form-item>
-              <el-tag
-                type="danger"
-                v-if="itemSelectedLesson==''"
-              >در حال حاظر درسی را انتخاب نکرده اید</el-tag>
+  
+              <el-tag type="danger" v-if="itemSelectedLesson==''">در حال حاظر درسی را انتخاب نکرده اید</el-tag>
+  
               <el-tag v-else>{{itemSelectedLesson}}</el-tag>
+  
             </el-form-item>
+  
             <el-form-item label="پیام به مدیریت">
+  
               <el-input type="textarea" v-model="form.teacherMessage"></el-input>
+  
             </el-form-item>
-
+  
+  
+  
             <el-form-item>
-              <input
-                type="file"
-                name="myfile"
-                @change="processFile"
-                ref="filePicker"
-                style="display: none"
-              >
+  
+              <input type="file" name="myfile" @change="processFile" ref="filePicker" style="display: none">
+  
               <el-button @click="$refs.filePicker.click()">انتخاب فایل</el-button>
+  
               <br>
-              <el-button
-                style="margin-left: 10px;color:#fff !important"
-                size="medium "
-                type="primary"
-                @click="registerLesson"
-              >ارسال</el-button>
-              
+  
+              <el-button style="margin-left: 10px;color:#fff !important" size="medium " type="primary" @click="registerLesson">ارسال</el-button>
+  
+  
+  
             </el-form-item>
+  
           </el-form>
+  
         </el-col>
+  
         <el-col :md="12">
-          <el-alert
-            title="توضیحات"
-            type="info"
-            description="در این بخش توضیحات قرار می گیرد"
-            :closable="false"
-            show-icon
-          ></el-alert>
+  
+          <el-alert title="توضیحات" type="info" description="در این بخش توضیحات قرار می گیرد" :closable="false" show-icon></el-alert>
+  
         </el-col>
+  
       </el-row>
+  
     </el-dialog>
-
+  
+  
+  
     <!-- searchLessonItem -->
-    <searchLesson
-      :isOpen="isLessonsearchDialog"
-      @close="selectedLesson=undefined"
-      v-on:lessonSelected="getItems($event)"
-      v-on:close="closeSearchgDialog"
-    ></searchLesson>
+  
+    <searchLesson :isOpen="isLessonsearchDialog" @close="selectedLesson=undefined" v-on:lessonSelected="getItems($event)" v-on:close="closeSearchgDialog"></searchLesson>
+  
   </div>
 </template>
 <script>
-import axios from "axios";
-
-import searchLesson from "./search-lesson";
-export default {
-  name: "add-course-dialog",
-  props: {
-    isOpen: {
-      type: Boolean
-    }
-  },
-  components: {
-    searchLesson
-  },
-  data() {
-    return {
-      isLessonsearchDialog: false,
-      selectedTeacherItem: "",
-      itemSelectedLesson: "",
-      selectedLesson: null,
-      close: false,
-      uploadProgress: 0,
-      courseTitles: [],
-      form: {
-        price: 0,
-        description: "",
-        courseTitle: "",
-        teacherMessage: "",
-        lessonFieldId: 0,
-        zipFile: ""
+  import axios from "axios";
+  
+  
+  
+  import searchLesson from "./search-lesson";
+  
+  export default {
+  
+    name: "add-course-dialog",
+  
+    props: {
+  
+      isOpen: {
+  
+        type: Boolean
+  
       }
-    };
-  },
-
-  methods: {
-    getCourseTitle() {
-      axios.get("/api/courseTitles").then(res => {
-        this.courseTitles = res.data;
-      });
+  
     },
-    getItems(item) {
-      this.itemSelectedLesson =
-        item.gradeName + " - " + item.fieldName + " - " + item.lessonName;
-      this.form.lessonFieldId = item.id;
-      console.log(item);
+  
+    components: {
+  
+      searchLesson
+  
     },
-    closeSearchgDialog() {
-      this.isLessonsearchDialog = false;
-    },
-    processFile(event) {
-      this.form.zipFile = event.target.files[0];
-    },
-    registerLesson() {
-      const config = {
-        onUploadProgress: function(progressEvent) {
-          this.fileUploadPercentCompleted = Math.round(
-            (progressEvent.loaded * 100) / progressEvent.total
-          );
+  
+    data() {
+  
+      return {
+  
+        isLessonsearchDialog: false,
+  
+        selectedTeacherItem: "",
+  
+        itemSelectedLesson: "",
+  
+        selectedLesson: null,
+  
+        close: false,
+  
+        uploadProgress: 0,
+  
+        courseTitles: [],
+  
+        form: {
+  
+          price: 0,
+  
+          description: "",
+  
+          courseTitle: "",
+  
+          teacherMessage: "",
+  
+          lessonFieldId: 0,
+  
+          zipFile: ""
+  
         }
+  
       };
-      let data = new FormData();
-      data.append("Price", this.form.price);
-      data.append("Description", this.form.description);
-      data.append("TeacherMessageForAdmin", this.form.teacherMessage);
-      data.append("CourseTitleId", this.form.courseTitle);
-      data.append("LessonFieldId", this.form.lessonFieldId);
-      data.append("ZipFile", this.form.zipFile);
-      axios
-        .post("/api/TeacherAccount/courses", data, {
-          headers: {
-            "Content-Type": "multipart/form-data"
-          },
-          onUploadProgress: progressEvent => {
-            this.uploadProgress = Math.floor(
-              (progressEvent.loaded * 100) / progressEvent.total
-            );
-          }
-        })
-        .then(res => {
-          if (res.data.status == 1) {
-            this.$message({
-              message: "دوره شما با موفقیت ثبت شد",
-              type: "success"
-            });
-            this.$emit('close');
-          }
+  
+    },
+  
+  
+  
+    methods: {
+  
+      getCourseTitle() {
+  
+        axios.get("/api/courseTitles").then(res => {
+  
+          this.courseTitles = res.data;
+  
         });
+  
+      },
+  
+      getItems(item) {
+  
+        this.itemSelectedLesson =
+  
+          item.gradeName + " - " + item.fieldName + " - " + item.lessonName;
+  
+        this.form.lessonFieldId = item.id;
+  
+        console.log(item);
+  
+      },
+  
+      closeSearchgDialog() {
+  
+        this.isLessonsearchDialog = false;
+  
+      },
+  
+      processFile(event) {
+  
+        this.form.zipFile = event.target.files[0];
+  
+      },
+  
+      registerLesson() {
+  
+        const config = {
+  
+          onUploadProgress: function(progressEvent) {
+  
+            this.fileUploadPercentCompleted = Math.round(
+  
+              (progressEvent.loaded * 100) / progressEvent.total
+  
+            );
+  
+          }
+  
+        };
+  
+        let data = new FormData();
+  
+        data.append("Price", this.form.price);
+  
+        data.append("Description", this.form.description);
+  
+        data.append("TeacherMessageForAdmin", this.form.teacherMessage);
+  
+        data.append("CourseTitleId", this.form.courseTitle);
+  
+        data.append("LessonFieldId", this.form.lessonFieldId);
+  
+        data.append("ZipFile", this.form.zipFile);
+  
+        axios
+  
+          .post("/api/TeacherAccount/courses", data, {
+  
+            headers: {
+  
+              "Content-Type": "multipart/form-data"
+  
+            },
+  
+            onUploadProgress: progressEvent => {
+  
+              this.uploadProgress = Math.floor(
+  
+                (progressEvent.loaded * 100) / progressEvent.total
+  
+              );
+  
+            }
+  
+          })
+  
+          .then(res => {
+  
+            if (res.data.status == 1) {
+  
+              this.$message({
+  
+                message: "دوره شما با موفقیت ثبت شد",
+  
+                type: "success"
+  
+              });
+  
+              this.$emit('close');
+  
+            }
+  
+          });
+  
+      }
+  
+    },
+  
+  
+  
+    mounted() {
+  
+      this.getCourseTitle();
+  
     }
-  },
-
-  mounted() {
-    this.getCourseTitle();
-  }
-};
+  
+  };
 </script>
 <style>
-.w100 {
-  width: 100%;
-}
-.el-upload,
-.el-upload-dragger {
-  width: 100%;
-}
-.a {
-  z-index: -1;
-}
-.white {
-  color: #fff !important;
-}
-.el-tag {
-  width: 100%;
-  text-align: center;
-  font-size: 14px;
-}
-.description-Added-Course {
-  margin-right: 16px;
-}
-.description-Added-Course .title-Description {
-  text-align: justify;
-  line-height: 10px;
-}
+  .w100 {
+  
+    width: 100%;
+  
+  }
+  
+  
+  
+  .el-upload,
+  
+  .el-upload-dragger {
+  
+    width: 100%;
+  
+  }
+  
+  
+  
+  .a {
+  
+    z-index: -1;
+  
+  }
+  
+  
+  
+  .white {
+  
+    color: #fff !important;
+  
+  }
+  
+  
+  
+  .el-tag {
+  
+    display: inline;
+  
+    float: left;
+  
+    text-align: center;
+  
+    font-size: 14px;
+  
+  }
+  
+  
+  
+  .description-Added-Course {
+  
+    margin-right: 16px;
+  
+  }
+  
+  
+  
+  .description-Added-Course .title-Description {
+  
+    text-align: justify;
+  
+    line-height: 10px;
+  
+  }
 </style>
 
--- Zanis.Ostad.Web/ClientApp/components/teacher/courses/index.vue
+++ Zanis.Ostad.Web/ClientApp/components/teacher/courses/index.vue
@@ -1,78 +1,78 @@
 <template>
   <el-card>
-     <h4 style="display:inline;">دروس تدریس شده</h4>
+    <h4 style="display:inline;">دروس تدریس شده</h4>
     <div>
       <el-button @click="isAddingNewCourse=true" class="left">افزودن دوره جدید</el-button>
     </div>
     <el-table height="500" :data="courceData" size="large" style="width: 100%">
-
-
-       <el-table-column label="عنوان" width="300">
-            <template slot-scope="scope">
-              {{ scope.row.title}}
-            </template>
+  
+  
+      <el-table-column label="عنوان" width="300">
+        <template slot-scope="scope">
+                                                      {{ scope.row.title}}
+</template>
       </el-table-column>
 
 
       <el-table-column label="مقطع" width="120">
-        <template slot-scope="scope">
-          {{ scope.row.gradeTitle}}
-        </template>
+<template slot-scope="scope">
+   {{ scope.row.gradeTitle}}
+</template>
       </el-table-column>
 
       <el-table-column label="رشته" width="120">
-        <template slot-scope="scope">
-          {{ scope.row.fieldName}}
-        </template>
+<template slot-scope="scope">
+   {{ scope.row.fieldName}}
+</template>
       </el-table-column>
 
          <el-table-column label="درس" width="120">
-        <template slot-scope="scope">
-          {{ scope.row.lessonTitle}}
-        </template>
+<template slot-scope="scope">
+   {{ scope.row.lessonTitle}}
+</template>
       </el-table-column>
 
     <el-table-column label="قیمت" width="90">
-        <template slot-scope="scope">
-          {{scope.row.price}}
-        </template>
+<template slot-scope="scope">
+   {{scope.row.price}}
+</template>
       </el-table-column>
 
       <el-table-column label="کددرس" width="90">
-        <template slot-scope="scope">
-          {{ scope.row.lessonCode}}
-        </template>
+<template slot-scope="scope">
+   {{ scope.row.lessonCode}}
+</template>
       </el-table-column>
 
        <el-table-column label="وضعیت" width="220">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.approvalStatus===0">در انتظار تایید</el-tag>
-          <el-tag v-if="scope.row.approvalStatus===5" type="success">تایید شده</el-tag>
-          <el-tag v-if="scope.row.approvalStatus===10" type="danger">رد شده</el-tag>
-          <el-tag v-if="scope.row.approvalStatus===15" type="warning">غیر فعال توسط مدرس</el-tag>
-        </template>
+<template slot-scope="scope">
+  <el-tag v-if="scope.row.approvalStatus===0">
+    در انتظار تایید</el-tag>
+  <el-tag v-if="scope.row.approvalStatus===5" type="success">تایید شده</el-tag>
+  <el-tag v-if="scope.row.approvalStatus===10" type="danger">رد شده</el-tag>
+  <el-tag v-if="scope.row.approvalStatus===15" type="warning">غیر فعال توسط مدرس</el-tag>
+</template>
       </el-table-column>
 
       <el-table-column label="جزئیات" width="150">
-        <template slot-scope="scope">
-          <el-button v-bind:disabled="scope.row.contents.length==0" @click="showDetails(scope.row.id)">
-            مشاهده</el-button>
-            
-        </template>
+<template slot-scope="scope">
+  <el-button v-bind:disabled="scope.row.contents.length==0" @click="selectedCourseId=scope.row.id">
+    مشاهده</el-button>
+</template>
       </el-table-column>
 
       
 
        <el-table-column label="عملیات">
-        <template slot-scope="scope">
-         <el-row type="flex">
-
-           <el-button @click="changingApprovalStateItem=scope.row" class="deactive">
-              تغییر وضعیت
-            </el-button>
-
-            </el-row>
-        </template>
+<template slot-scope="scope">
+  <el-row type="flex">
+  
+    <el-button @click="changingApprovalStateItem=scope.row" class="deactive">
+      تغییر وضعیت
+    </el-button>
+  
+  </el-row>
+</template>
       </el-table-column>
 
     </el-table>
@@ -87,37 +87,9 @@
       :total="meta.allCount"
     ></el-pagination>
 
-    <el-dialog title="جزئیات دوره" :visible.sync="courcedialog" width="80%">
-      <el-row :gutter="40">
-        <el-col :xs="12" :md="12" :lg="12" v-for="(item,index) in courcedetails" :key="index">
-         
-           <el-card  class="card-item">
-            <div slot="header" class="clearfix">
-              <span>{{item.title}}</span>
-              <span class="icon" v-html="previewIconCourse(item.contentType)"></span>
-              
-              <el-tag v-if="item.state==5" type="success" class="icon">تایید شده</el-tag>
-              <el-tag v-else-if="item.state==0" class="icon">در انتظار تایید</el-tag>
-              <el-tag v-else-if="item.state==10" type="danger" class="icon">رد شده</el-tag>
-              <el-tag v-else-if="item.state==15" type="info" class="icon">رد شده توسط استاد</el-tag>
+    <CourseDetails v-if="selectedCourseId" :isOpen="selectedCourseId" :courseId="selectedCourseId" @close="selectedCourseId=undefined"></CourseDetails>
 
-            </div>
-
-            <div class="wrapper-body-card">
-              {{item.adminMessageForTeacher}}
-            </div>
-
-            <div class="wrapper-download-link">
-
-                <el-button type="primary" round class="downloadBtn">
-                  <i class="fas fa-download customDownloadIcon"></i>
-                   <a :href="''+item.filePath+''" class="white">دانلود</a>
-                </el-button>
-                         </div>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-dialog>
+    
     <add-course @close="isAddingNewCourse=false" :isOpen="isAddingNewCourse"></add-course>
     <approval-state-changer 
                           :isOpen="changingApprovalStateItem"
@@ -129,7 +101,8 @@
 
 <script>
   import AddCourse from './add-course-dialog';
-  import ApprovalStateChanger from './approval-state-changer'
+  import ApprovalStateChanger from './approval-state-changer';
+  import CourseDetails from './course-details.vue';
   import axios from "axios";
   export default {
     name: "AdminListCourse",
@@ -139,27 +112,29 @@
           pageSize: 10
         },
         courceData: [],
-        isAddingNewCourse:false,
+        isAddingNewCourse: false,
         courcedetails: [],
         courcedialog: false,
-        changingApprovalStateItem:false,
+        selectedCourseId: false,
+        changingApprovalStateItem: false,
         meta: {}
       };
-
+  
     },
-     components:{
-        ApprovalStateChanger,
-         AddCourse
-       },
+    components: {
+      ApprovalStateChanger,
+      AddCourse,
+      CourseDetails
+    },
     methods: {
       getCourse() {
-               axios.get("/api/Courses", {
+        axios.get("/api/Courses", {
           params: this.query
         }).then(res => {
-         console.log(res);
+  
           this.courceData = res.data.items;
           this.meta = res.data.allCount;
-                });
+        });
       },
       handleSizeChange(val) {
         this.query.pageSize = val;
@@ -170,57 +145,12 @@
         this.query.currentPage = val;
         this.getCourse();
       },
-
-      showDetails(id) {
-        axios
-          .get("/api/Courses/" + id)
-          .then(res => {
-            this.courcedetails = res.data.contents;
-            this.courcedialog = true;
-            console.log(res.data.contents);
-                    })
-          .catch(err => {});
-      },
-      previewIconCourse(contentType) {
-      switch (contentType) {
-          case 0:
-            return '<i class="far fa-file-pdf"></i>';
-          case 1:
-            return '<i class="far fa-file-video"></i>';
-            default:
-            return '';
-        }
-      },
-      activeCourse(id){
-        axios.patch('/api/TeacherAccount/courses/'+id+'/active')
-        .then(res=>{
-        this.$message({
-          message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
-          type: 'success'
-        });
-        })
-        .catch(err=>{
-
-        })
-
-      },
-      deActiveCourse(id){
-
-         axios.patch('/api/TeacherAccount/courses/'+id+'/deactive')
-        .then(res=>{
-          this.$message({
-          message: 'برای تغییر وضعیت این مورد ابتدا محتوا میبایست توسط مدیر سیستم تعیین وضعیت شود',
-          type: 'warning'
-        });
-      
-        })
-        .catch(err=>{
-
-        })
-
-      }
+  
+  
+  
+  
     },
-   
+  
     mounted() {
       this.getCourse();
     }
@@ -228,33 +158,32 @@
 </script>
 
 <style scoped>
-  .icon {
-    float: left;
-  }
-
   .downloadBtn {
-        float: left;
-        margin-bottom: 10px;
+    float: left;
+    margin-bottom: 10px;
   }
-
-  .card-item{
+  
+  .card-item {
     margin-bottom: 50px;
   }
-
-  .deactive{
+  
+  .deactive {
     margin-right: 0;
   }
-  .customDownloadIcon{
+  
+  .customDownloadIcon {
     margin: 0;
     line-height: 8px;
     padding-left: 5px;
     color: #fff;
     font-size: 14px;
   }
-  .mgl-17{
+  
+  .mgl-17 {
     margin-left: 17px;
   }
-.left {
-  float: left  !important;
-}
+  
+  .left {
+    float: left !important;
+  }
 </style>
