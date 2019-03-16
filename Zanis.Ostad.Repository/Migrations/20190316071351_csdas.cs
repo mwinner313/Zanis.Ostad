9dd70a695b000ca09fbacd88c492bccf05edc89a
golzar <a.golzarzadeh@gmail.com>
2019-03-16 15:50:35 +0330

    wip

---
+++ Zanis.Ostad.Repository/Migrations/20190316071351_csdas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zanis.Ostad.Repository.Migrations
+{
+    public partial class csdas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PendingToApproveItemsCount",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Contents");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasPendingToApproveItem",
+                table: "Courses",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasPendingToApproveItem",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PendingToApproveItemsCount",
+                table: "Courses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Contents",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Zanis.Ostad.Repository/Migrations/20190316071827_csdas2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zanis.Ostad.Repository.Migrations
+{
+    public partial class csdas2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasPendingToApproveItem",
+                table: "Courses",
+                newName: "HasPendingItemToApprove");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HasPendingItemToApprove",
+                table: "Courses",
+                newName: "HasPendingToApproveItem");
+        }
+    }
+}
