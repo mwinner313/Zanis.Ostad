49febfd966f876adf02b5d6aa9171c3fe34b7d08
golzar <a.golzarzadeh@gmail.com>
2019-03-17 13:24:28 +0330

    wip

--- Zanis.Ostad.Web/Controllers/Api/EditorAccountController.cs
+++ Zanis.Ostad.Web/Controllers/Api/EditorAccountController.cs
@@ -1,11 +1,26 @@
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Zains.Ostad.Application.Editors.Queries.GetEditAssignments;
+using Zains.Ostad.Application.Infrastucture;
+
 namespace Zanis.Ostad.Web.Controllers.Api
 {
     [Authorize(Roles = "Admin,Editor")]
     [Route("api/[controller]")]
     public class EditorAccountController : Controller
     {
-       // public async Task<ActionResult<PagenatedList<>>> Get()
+        private readonly IMediator _mediator;
+
+        public EditorAccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<PagenatedList<EditAssignmentViewModel>>> Get(GetEditAssignmentsQuery query)
+        {
+            return await _mediator.Send(query);
+        }
     }
 }
