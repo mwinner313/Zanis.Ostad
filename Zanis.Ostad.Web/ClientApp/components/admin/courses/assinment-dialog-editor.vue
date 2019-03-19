1acb5c6c7b66674bf008a564d75a26560fbabbe5
SyedAliHossini <salihoosainione@gmail.com>
2019-03-19 16:03:59 +0330

    wip

--- Zanis.Ostad.Web/ClientApp/components/admin/courses/assinment-dialog-editor.vue
+++ Zanis.Ostad.Web/ClientApp/components/admin/courses/assinment-dialog-editor.vue
@@ -6,26 +6,68 @@
     @beforeClose="$emit('close')"
     width="60%"
   >
-    <el-table :data="courseData.contents" style="width: 100%">
-      <el-table-column label="ردیف">
-        <template slot-scope="scope">{{scope.row.id}}</template>
-      </el-table-column>
-      <el-table-column label="تاریخ">
-        <template slot-scope="scope">{{scope.row.createdOn| moment("jYYYY/jM/jD HH:mm")}}</template>
-      </el-table-column>
-      <el-table-column label="عنوان">
-        <template slot-scope="scope">{{scope.row.title}}</template>
-      </el-table-column>
-    </el-table>
+    <el-row :gutter="20">
+      <el-col :md="18" :lg="18">
+        <el-card shadow="never">
+          <h5 style="text-align:center">سرفصلها</h5>
+          <el-table :data="courseData.contents" style="width: 100%">
+            <el-table-column>
+              <template slot-scope="scope">
+                <el-checkbox
+                  @change="selected"
+                  name="type"
+                  :label="scope.row.id"
+                  v-model="selectedItemIds"
+                  border
+                >{{scope.row.title}}</el-checkbox>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+      <el-col :md="6" :lg="6">
+        <el-card shadow="never">
+          <h5 style="text-align:center">تدوینگرها</h5>
+          <el-form :inline="true">
+            <el-form-item>
+              <el-input @change="getEditorList" placeholder="جستجو" v-model="query.search"></el-input>
+            </el-form-item>
+          </el-form>
+          <el-table :data="editorList.items" style="width: 100%">
+            <el-table-column>
+              <template slot-scope="scope">{{scope.row.fullName}}</template>
+            </el-table-column>
+            <el-table-column>
+              <template slot-scope="scope">
+                <el-button
+                  type="primary"
+                  v-show="selectedItemIds.length"
+                  @click="checkSelectedItem(scope.row.id)"
+                  class="white"
+                >انتخاب</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+    </el-row>
   </el-dialog>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
-      courseData: []
+      courseData: [],
+      selectedItemIds: [],
+      editorList: {},
+      query: {
+        search: "",
+        pageSize: 10
+      },
+      editorId: 0
     };
   },
   props: {
@@ -38,6 +80,7 @@ export default {
   },
   mounted() {
     this.loadData();
+    this.getEditorList();
   },
   methods: {
     loadData() {
@@ -45,13 +88,46 @@ export default {
         .get("/api/Courses/" + this.courseId)
         .then(res => {
           this.courseData = res.data;
-          console.log(res.data.contents);
+          this.courseData.contents = this.courseData.contents.filter(
+            p => p.contentType == 0
+          );
         })
         .catch(err => {});
+    },
+    getEditorList() {
+      axios.get("/api/Editors", { params: this.query }).then(res => {
+        this.editorList = res.data;
+      });
+    },
+    selected(e) {
+      console.log(this.selectedItemIds);
+    },
+    checkSelectedItem(editorId) {
+      if (this.selectedItemIds.length == 0) {
+        this.$message.error("سرفصلی را انتخاب نکرده اید");
+        return;
+      }
+
+      axios
+        .post("/api/EditAssignment/bycourseitem", {
+          editorId,
+          courseItemIds: this.selectedItemIds
+        })
+        .then(res => {
+          if (res.data.status == 1) {
+            this.$message({
+              type: "success",
+              message: "عملیات با موفقیت انجام شد"
+            });
+          }
+        });
     }
   }
 };
 </script>
 
 <style>
+.white {
+  color: #fff !important;
+}
 </style>
