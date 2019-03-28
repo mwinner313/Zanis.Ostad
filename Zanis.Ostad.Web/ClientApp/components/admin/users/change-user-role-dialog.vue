7925e35037b5c68722933d31371c0bdb9d261690
SyedAliHossini <salihoosainione@gmail.com>
2019-03-28 10:51:13 +0430

    wio

--- Zanis.Ostad.Web/ClientApp/components/admin/users/change-user-role-dialog.vue
+++ Zanis.Ostad.Web/ClientApp/components/admin/users/change-user-role-dialog.vue
@@ -1,73 +1,65 @@
 <template>
-  <el-dialog
-    :title="item.title"
-    :visible.sync="isOpen"
-    width="40%"
-    @closed="$emit('close')"
-   >
+  <el-dialog :title="item.title" :visible.sync="isOpen" width="40%" @closed="$emit('close')">
     <el-form ref="form" :model="form">
-      <el-form-item prop="roles" label="انتخاب نقش ها">
-
-      </el-form-item>
-      <el-checkbox
-        v-for="role in roles"
-        name="type"
-        :label="role.name"
-        v-model="user.roles"
-        border>
-      </el-checkbox>
+      <el-form-item prop="roles" label="انتخاب نقش ها"></el-form-item>
+      <el-checkbox v-for="role in roles" name="type" :label="role.name" v-model="user.roles" border></el-checkbox>
     </el-form>
     <span slot="footer" class="dialog-footer">
-    <el-button @click="$emit('close')">بستن</el-button>
-    <el-button type="primary" @click="submit">ثبت</el-button>
-  </span>
+      <el-button @click="$emit('close')">بستن</el-button>
+      <el-button type="primary" @click="submit">ثبت</el-button>
+    </span>
   </el-dialog>
 </template>
 
 <script>
-  import axios from 'axios'
-  import EventBus from './../../../event-bus'
+import axios from "axios";
+import EventBus from "./../../../event-bus";
 
-  export default {
-    name: "",
-    props: {
-      isOpen: {
-        type: Boolean
-      },
-      item: {
-        type: Object
-      }
+export default {
+  name: "",
+  props: {
+    isOpen: {
+      type: Boolean
     },
-    data() {
-      return {
-        form: {},
-        roles:[],
-        user:{roles:[]}
-      }
-    },
-    mounted(){
-      this.user=Object.assign({},this.item);
-      axios.get('/api/admin/roles').then(res=>{
-        this.roles=res.data;
-      });
-    },
+    item: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      form: {},
+      roles: [],
+      user: { roles: [] }
+    };
+  },
+  mounted() {
+    this.user = Object.assign({}, this.item);
+    axios.get("/api/admin/roles").then(res => {
+      this.roles = res.data;
+    });
+  },
 
-    methods: {
-      submit() {
-        this.$refs.form.validate((valid) => {
-          if (valid) {
-            axios.post('/api/Admin/change_user_roles', {userId: this.item.id, roles: this.user.roles}).then(res => {
-              this.$emit('close',{reloadData:true});
+  methods: {
+    submit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          axios
+            .post("/api/Admin/change_user_roles", {
+              userId: this.item.id,
+              roles: this.user.roles
+            })
+            .then(res => {
+              this.$emit("close", { reloadData: true });
             });
-          }
-        });
-      },
+        }
+      });
     }
   }
+};
 </script>
 
 <style scoped>
-  .el-select {
-    width: 100%;
-  }
+.el-select {
+  width: 100%;
+}
 </style>
